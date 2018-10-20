eca9c2ce15dde394563782d92187d88afbf61186
EpicPi <piyushgk1@gmail.com>
2018-10-20 19:28:34 -0400

    table info has been gotten

--- frontend/actions/index.js
+++ frontend/actions/index.js
@@ -1,11 +1,10 @@
 // Action Creators
 
-import {LOGIN, SEARCHTICKER} from './types';
+import {LOGIN, SEARCHTICKER,GET_TABLE_INFO} from './types';
 
 import axios from 'axios/index';
 
 export const login = (uname, pword) => async dispatch => {
-
     const data = {
         username: uname,
         password: pword,
@@ -19,16 +18,15 @@ export const login = (uname, pword) => async dispatch => {
     console.log(res.data);
     
     dispatch({ type: LOGIN, payload: res.data });
-    
 };
 
 export const searchTicker = (ticker) => async dispatch=> {
-    console.log("found method");
     const params = {
         chain_symbol: ticker,
     };
     const r = await axios.get("https://api.robinhood.com/options/instruments/", {params: params});
     const chain_id = r.data.results[0].chain_id;
+    
     const params2 = {
         ids: chain_id
     };
@@ -37,6 +35,7 @@ export const searchTicker = (ticker) => async dispatch=> {
 
     const pricer = await axios.get("https://api.robinhood.com/quotes/" + ticker+'/');
     const stockPrice = pricer.data.last_traded_price;
+    
     const payload = {
         ticker: ticker,
         dates: expirationDates,
@@ -45,3 +44,30 @@ export const searchTicker = (ticker) => async dispatch=> {
     dispatch({type: SEARCHTICKER, payload: payload});
 };
 
+export const getTableInfo = (token, ticker, optionType, expirationDate) => async dispatch =>{
+    const option_type =  optionType.toLowerCase()
+    let params ={
+        chain_symbol: ticker,
+        type: option_type,
+        state: "active",
+        expiration_dates: expirationDate
+    }
+    const r = await axios.get("https://api.robinhood.com/options/instruments/",{params:params});
+    const results = r.data.results.map(res=>({url: res.url, strikePrice: res.strike_price}));
+    const instruments = results.map(res => res.url).reduce((comb, url) => {
+        return comb + ',' + url
+    }, '').substring(1);
+
+    params = {
+        instruments: instruments
+    }
+    const r2 = await axios.get("https://api.robinhood.com/marketdata/options/",{ headers: {"Authorization" : `Bearer ${token}`},params: params});
+    const results2 = r2.data.results
+
+
+    const payload = results.map(res =>  {
+        return Object.assign({}, res, {cost: results2.find(r => r.instrument === res.url).adjusted_mark_price});
+    });
+    
+    dispatch({type:GET_TABLE_INFO, payload: payload})
+};
