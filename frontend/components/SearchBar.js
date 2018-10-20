8a33213fd5597e5b7a17e0e690c6610d6110e55e
Sammy Baek <kie827@gmail.com>
2018-10-20 14:57:01 -0400

    implement search

--- frontend/components/SearchBar.js
+++ frontend/components/SearchBar.js
@@ -3,10 +3,48 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ticker: '',
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleGeneral = this.handleGeneral.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        // this.props.searchTicker()
+
+        this.setState({ ticker: '' });
+    }
+
+    handleGeneral(e) {
+        this.setState({ [e.target.name]: e.target.value});
+    }
+
     render() {
         return (
             <div>
-                SearchBar
+                <form className="form-inline" onSubmit={this.handleSubmit}>
+                    <input
+                        className="form-control form-control-sm mr-3 w-75"
+                        type="text"
+                        placeholder="AAPL"
+                        aria-label="Search"
+                        name="ticker"
+                        value={this.state.ticker}
+                        onChange={this.handleGeneral}
+                        required
+                    />
+                    <button
+                        className="btn btn-success"
+                        type="submit"
+                    >
+                    Search
+                    </button>
+                </form>
             </div>
         );
     }
