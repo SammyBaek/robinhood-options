f94a19a6263826f6225bba496f4774af5768413c
EpicPi <piyushgk1@gmail.com>
2018-10-20 17:35:21 -0400

    searchTicker method

--- frontend/actions/index.js
+++ frontend/actions/index.js
@@ -1,12 +1,11 @@
 // Action Creators
 
-import {LOGIN} from './types';
+import {LOGIN, SEARCHTICKER} from './types';
 
 import axios from 'axios/index';
 
 export const login = (uname, pword) => async dispatch => {
-    console.log("found method");
-    // axios.post('url', data);
+
     const data = {
         username: uname,
         password: pword,
@@ -18,5 +17,30 @@ export const login = (uname, pword) => async dispatch => {
     };
     const res = await axios.post("https://api.robinhood.com/oauth2/token/", data);
     console.log(res.data);
+    
     dispatch({ type: LOGIN, payload: res.data });
+    
+};
+
+export const searchTicker = (ticker) => async dispatch=> {
+    console.log("found method");
+    const params = {
+        chain_symbol: ticker,
+    };
+    const r = await axios.get("https://api.robinhood.com/options/instruments/", {params: params});
+    const chain_id = r.data.results[0].chain_id;
+    const params2 = {
+        ids: chain_id
+    };
+    const datesr = await axios.get("https://api.robinhood.com/options/chains/", {params: params2});
+    const expirationDates = datesr.data.results[0].expiration_dates;
+
+    const pricer = await axios.get("https://api.robinhood.com/quotes/" + ticker+'/');
+    const stockPrice = pricer.data.last_traded_price;
+    const payload = {
+        ticker: ticker,
+        dates: expirationDates,
+        stockPrice: stockPrice
+    };
+    dispatch({type: SEARCHTICKER, payload: payload});
 };
